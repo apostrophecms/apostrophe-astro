3fa86b9beed749c0e334359554e8537977fdaa20
Thomas Boutell <tom@apostrophecms.com>
2023-12-08 14:40:32 -0500

    a lot of code cleanup, documentation cleanup WIP, and WIP fix for pro-5248

--- endpoints/aposProxy.js
+++ endpoints/aposProxy.js
@@ -1,11 +1,9 @@
-import aposResponse from "../lib/aposResponse"
+import aposResponse from "../lib/aposResponse";
 
 export async function ALL({ params, request }) {
-    let response
-    try {
-      response = await aposResponse(request)
-    } catch (e) {
-      response = new Response(e.message, { status: 500 })
-    }
-    return response
+  try {
+    return await aposResponse(request);
+  } catch (e) {
+    return new Response(e.message, { status: 500 });
   }
+};
--- vite/vite-plugin-apostrophe-config.js
+++ vite/vite-plugin-apostrophe-config.js
@@ -1,20 +1,20 @@
 export function vitePluginApostropheConfig(aposHost, forwardHeaders) {
 
-    const virtualModuleId = "virtual:apostrophe-config";
-    const resolvedVirtualModuleId = "\0" + virtualModuleId;
+  const virtualModuleId = "virtual:apostrophe-config";
+  const resolvedVirtualModuleId = "\0" + virtualModuleId;
 
-    return {
-        name: "vite-plugin-apostrophe-config",
-        async resolveId(id) {
-            if (id === virtualModuleId) {
-                return resolvedVirtualModuleId;
-            }
-        },
-        async load(id) {
-            if (id === resolvedVirtualModuleId) {
-                    return `
-                    export default { aposHost: "${aposHost}" ${forwardHeaders ? `, forwardHeaders: ${JSON.stringify(forwardHeaders)}` : ''} }`
-            }
-        },
-    };
-}
+  return {
+    name: "vite-plugin-apostrophe-config",
+    async resolveId(id) {
+      if (id === virtualModuleId) {
+        return resolvedVirtualModuleId;
+      }
+    },
+    async load(id) {
+      if (id === resolvedVirtualModuleId) {
+          return `
+          export default { aposHost: "${aposHost}" ${forwardHeaders ? `, forwardHeaders: ${JSON.stringify(forwardHeaders)}` : ''} }`
+      }
+    },
+  };
+};
