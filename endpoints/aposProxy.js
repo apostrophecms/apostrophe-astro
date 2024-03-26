c71414391008a40c5449d3c997c839ef43534e77
Thomas Boutell <tom@apostrophecms.com>
2024-03-26 15:31:38 -0400

    wip

--- endpoints/aposProxy.js
+++ endpoints/aposProxy.js
@@ -1,8 +1,8 @@
 import aposResponse from "../lib/aposResponse";
 
-export async function ALL({ params, request }) {
+export async function ALL({ params, request, redirect }) {
   try {
-    return await aposResponse(request);
+    return await aposResponse(request, { redirect });
   } catch (e) {
     return new Response(e.message, { status: 500 });
   }
